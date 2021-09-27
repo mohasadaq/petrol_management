d1847e7361c515af0c4e21fefc367d929bc39ed0
mohasadaq <85059435+mohasadaq@users.noreply.github.com>
2021-09-27 15:15:24 +0300

    Update VendorRepo.java

--- src/main/java/com/uct/edu/so/petrol_management/Repo/VendorRepo.java
+++ src/main/java/com/uct/edu/so/petrol_management/Repo/VendorRepo.java
@@ -12,8 +12,8 @@ import java.util.Map;
 public interface VendorRepo extends JpaRepository<VendorModal,Integer> {
     @Transactional
     @Modifying
-    @Query(value = "UPDATE `vendor` SET `v_address`=?1\n" +
-            ",`v_email`=?2,`v_name`=?3,`v_phone`=?4 WHERE v_id=?5",nativeQuery = true)
+    @Query(value = "UPDATE vendor SET v_address=?1\n" +
+            ",v_email=?2,v_name=?3,v_phone=?4 WHERE v_id=?5",nativeQuery = true)
     void updateVendor(String address, String email,String name,String phone,int id);
 
     //UPDATE BALANCE
@@ -44,18 +44,18 @@ public interface VendorRepo extends JpaRepository<VendorModal,Integer> {
     // DELETE VENDOR STATEMENT
     @Transactional
     @Modifying
-    @Query(value = "DELETE FROM `venderstatement` WHERE dueTo=?1 and dueToId=?2",nativeQuery = true)
+    @Query(value = "DELETE FROM venderstatement WHERE dueTo=?1 and dueToId=?2",nativeQuery = true)
     void deleteVendorStatement(String dueto,int id);
     //
 
     @Transactional
     @Modifying
-    @Query(value = "INSERT INTO `venderstatement`(`vId`, `cashIn`, `cashOut`, `dueTo`,dueToId) \n" +
+    @Query(value = "INSERT INTO venderstatement(vId, cashIn, cashOut, dueTo,dueToId) \n" +
             "    VALUES (?1,?2,?3,?4,?5 )",nativeQuery = true)
     void vendorStatement(int id,double cashIn,double cashOut,String dueto,int dueToId);
 
     //GET VENDER SATEMENT BY PURCHASE
-    @Query(value = "SELECT * FROM `venderstatement` WHERE dueTo=?1 and dueToId = ?2",nativeQuery = true)
+    @Query(value = "SELECT * FROM venderstatement WHERE dueTo=?1 and dueToId = ?2",nativeQuery = true)
     List<Map<String,String>> getVendorStatement(String dueto, int dueToId);
 
     @Query(value = "SELECT v_id, vendor.v_name,ifnull(petroll_type.p_type,0)as p_type, ifnull(purchase_petrol.pid,0)as pid,ifnull(round(((quantity*purchase_petrol.price_per_liter)- purchase_petrol.amount_paid),2),0) as amountRemaining,IFnull(purchase_petrol.quantity*purchase_petrol.price_per_liter,0)as totalAmount,ifnull(purchase_petrol.amount_paid,0)as  amountpaid,ifnull(purchase_petrol.quantity,0) as numofliter,vendor.v_begining_balance\n" +
@@ -69,7 +69,7 @@ public interface VendorRepo extends JpaRepository<VendorModal,Integer> {
 
 
     //vendor that purchased from
-    @Query(value = "SELECT * FROM `venderstatement` WHERE vId=?1",nativeQuery = true)
+    @Query(value = "SELECT * FROM venderstatement WHERE vId=?1",nativeQuery = true)
     List<Map<String,String>> getVendorWhoPurchasedFrom(int vendorId);
 
 
